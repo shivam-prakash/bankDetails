8ee8e3934ea30d0dcc512e8cc25d10bed21bea9f
shivam <shivam.praksh@augmentolabs.com>
2020-03-22 18:53:54 +0530

    commit3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BankdetailsComponent } from './bankdetails/bankdetails.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'bankdetails', component: BankdetailsComponent },
+{ path: 'analytics', component: AnalyticsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BankdetailsComponent } from './bankdetails/bankdetails.component';
+import { BankService } from './service/bank.service';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter'; //importing the module
+import { Ng2OrderModule } from 'ng2-order-pipe'; //importing the module
+import { CommonModule } from '@angular/common';
+import { ChartsModule } from 'ng2-charts';
+import { AnalyticsComponent } from './analytics/analytics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankdetailsComponent,
+    AnalyticsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, HttpClientModule,
+    NgxPaginationModule, Ng2SearchPipeModule,
+    Ng2OrderModule, FormsModule, CommonModule, ChartsModule
   ],
-  providers: [],
+  providers: [BankService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
